8e6271a396d43fd8432cd53672155847d52996a7
Harlan T Wood <code@harlantwood.net>
2020-09-19 22:48:52 -1000

    Blog posts on homepage

---
+++ web/src/components/BlogPostsCell/BlogPostsCell.js
@@ -0,0 +1,25 @@
+export const QUERY = gql`
+  query BlogPostsQuery {
+    posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => <div>Error: {error.message}</div>
+
+export const Success = ({ posts }) => {
+  return posts.map((post) => (
+    <article key={post.id}>
+      <h2>{post.title}</h2>
+      <div>{post.body}</div>
+    </article>
+  ))
+}
